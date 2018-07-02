f97c3ae5d6a7e9982101abb6990754df564f9808
“sloutmyv” <clercsyl9123@gmail.com>
2018-07-02 18:19:20 +0200

    002 - Nav bar, statics and CSS costumization

--- APP_000_MAIN/settings/base_settings.py
+++ APP_000_MAIN/settings/base_settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    
+
     'APP_001_LANDINGPAGE',
 ]
 
@@ -120,3 +120,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR,"static"),
+    # 'var/www/static',
+]
+
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static_cdn")
+MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "media_cdn")
--- APP_001_LANDINGPAGE/views.py
+++ APP_001_LANDINGPAGE/views.py
@@ -5,3 +5,8 @@ from django.views.generic.base import TemplateView
 class LandingPageView(TemplateView):
 
     template_name = "APP_001_LANDINGPAGE/home.html"
+
+    def get_context_data(self, **kwargs):
+        context = super(LandingPageView, self).get_context_data(**kwargs)
+        context['title'] = "Home"
+        return context
