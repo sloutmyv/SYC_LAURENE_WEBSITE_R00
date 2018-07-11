6170c1bf11c6eea5818a14c83991037e98e98aba
“sloutmyv” <clercsyl9123@gmail.com>
2018-07-11 21:24:03 +0200

    012 - 20180711-1

--- APP_001_CLIENTPAGE/models.py
+++ APP_001_CLIENTPAGE/models.py
@@ -1,3 +1,16 @@
+from django.core.validators import RegexValidator
 from django.db import models
 
-# Create your models here.
+class Praticien(models.Model):
+    praticien_first_name   = models.CharField(max_length=120)
+    praticien_last_name    = models.CharField(max_length=120)
+    phone_regex            = RegexValidator(regex=r'^0\d{9}$', message='Phone number must be like 06XXXXXXXX')
+    praticien_phone_number = models.CharField(validators=[phone_regex], max_length=10)
+    praticien_email        = models.EmailField(max_length=254)
+    praticien_job          = models.CharField(max_length=120)
+    praticien_image        = models.ImageField(null=True,blank=True)
+    praticien_introduction = models.TextField(null=True,blank=True)
+    praticien_formation    = models.TextField(null=True,blank=True)
+
+    def __str__(self):
+        return self.praticien_first_name + ' ' + self.praticien_last_name
--- APP_001_CLIENTPAGE/views.py
+++ APP_001_CLIENTPAGE/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.views.generic.base import TemplateView
 
+from .models import Praticien
+
 class ClientPageView(TemplateView):
 
     template_name = "APP_001_CLIENTPAGE/home.html"
@@ -8,4 +10,5 @@ class ClientPageView(TemplateView):
     def get_context_data(self, *args, **kwargs):
         context = super(ClientPageView, self).get_context_data(*args, **kwargs)
         context['title'] = "Home"
+        context['praticien'] = Praticien.objects.all().first()
         return context
