8324a4d5441c47ef1f34dcfcf609f915d7fa9c56
“sloutmyv” <clercsyl9123@gmail.com>
2018-07-25 19:01:22 +0200

    20170725

--- APP_002_PROPAGE/forms.py
+++ APP_002_PROPAGE/forms.py
@@ -1,31 +1,44 @@
 from django import forms
 
+
 from .models import Patient
 
 class PatientCreateForm(forms.ModelForm):
+    patient_birthday = forms.DateField(required=False, widget=forms.DateInput(format='%d/%m/%Y'),input_formats=('%d/%m/%Y', ))
+
     class Meta:
         model = Patient
         fields = [
         'patient_first_name',
         'patient_last_name',
+        'patient_maiden_name',
+        'patient_birthday',
         'patient_phone_number',
         'patient_address',
         'patient_zip_code',
         'patient_city',
         'patient_contry',
-        'patient_note',
+        'patient_situation',
+        'patient_job',
+        'patient_gyn',
+        'patient_med',
         ]
 
     def __init__(self, *args, **kwargs):
         super(PatientCreateForm, self).__init__(*args, **kwargs)
         self.fields['patient_first_name'].label = "Prénom(s)"
         self.fields['patient_last_name'].label = "Nom(s) de famille"
+        self.fields['patient_maiden_name'].label = "Nom(s) de jeune fille"
+        self.fields['patient_birthday'].label = "Date de naissance"
         self.fields['patient_phone_number'].label = "Téléphone"
         self.fields['patient_address'].label = "Adresse"
         self.fields['patient_zip_code'].label = "Code postal"
         self.fields['patient_city'].label = "Ville"
         self.fields['patient_contry'].label = "Pays"
-        self.fields['patient_note'].label = "Note(s) générale(s)"
+        self.fields['patient_situation'].label = "Situation personnelle"
+        self.fields['patient_job'].label = "Profession"
+        self.fields['patient_gyn'].label = "Gynécologue"
+        self.fields['patient_med'].label = "Médecin traitant"
 
     # def clean_patient_first_name(self):
     #     patient_first_name = self.cleaned_data.get("patient_first_name")
--- APP_002_PROPAGE/models.py
+++ APP_002_PROPAGE/models.py
@@ -8,16 +8,58 @@ from APP_001_CLIENTPAGE.utils import unique_slug_generator
 class Patient(models.Model):
     patient_first_name         = models.CharField(max_length=120,verbose_name="Prénom")
     patient_last_name          = models.CharField(max_length=120,verbose_name="Nom")
+    patient_maiden_name        = models.CharField(max_length=120,verbose_name="Nom de jeune fille", null=True, blank=True)
+    patient_birthday           = models.DateTimeField(null=True,blank=True)
+
     phone_regex                = RegexValidator(regex=r'^0\d{9}$', message='Phone number must be like 06XXXXXXXX')
     patient_phone_number       = models.CharField(validators=[phone_regex], max_length=10, verbose_name="Téléphone")
+
+    # Adresse
+    patient_address            = models.CharField(max_length=300, verbose_name="Adresse", null=True, blank=True)
+    patient_zip_code           = models.CharField(max_length=5, verbose_name="Code postal", null=True, blank=True)
+    patient_city               = models.CharField(max_length=64, verbose_name="Ville", null=True, blank=True)
+    patient_contry             = models.CharField(max_length=64, default="France", verbose_name="Pays",null=True, blank=True)
+
+    SITUATIONS                 = (
+                                  ("celibataire","Célibataire"),
+                                  ("en_couple","En couple"),
+                                  ("mariee","Mariée")
+                                  )
+    patient_situation          = models.CharField(max_length=100,choices=SITUATIONS, default="celibataire", verbose_name="Situation familiale",null=True, blank=True)
+    patient_job                = models.CharField(max_length=120,verbose_name="Profession", null=True, blank=True)
+    patient_gyn                = models.CharField(max_length=200,verbose_name="Gynécologue", null=True, blank=True)
+    patient_med                = models.CharField(max_length=200,verbose_name="Médecin traitant", null=True, blank=True)
+
+    patient_taille             = models.FloatField(verbose_name="Taille (m)",null=True, blank=True)
+    patient_poid               = models.IntegerField(verbose_name="Poids (Kg)", null=True, blank=True)
+
+    patient_ant_fam            = models.TextField(verbose_name="Antécédants familiaux", null=True, blank=True)
+    patient_ant_perso          = models.TextField(verbose_name="Antécédants personnels", null=True, blank=True)
+    patient_ant_chir           = models.TextField(verbose_name="Antécédants chirurgicaux", null=True, blank=True)
+    patient_ant_gyn            = models.TextField(verbose_name="Antécédants gynécologiques", null=True, blank=True)
+
+
+    CONTRACEPTIONS             = (
+                                  ("rien","Pas de contraception"),
+                                  ("anneau_vaginal","Anneau vignal"),
+                                  ("DIU","DIU"),
+                                  ("implant","Implant contraceptif"),
+                                  ("ligature","Ligature des trompes"),
+                                  ("obturation","Obturation des trompes"),
+                                  ("patch","Patch"),
+                                  ("pilule","Pilule contraceptive"),
+                                  ("progestatif_en_injection","Progestatifs en injections"),
+                                  )
+    patient_contraception      = models.CharField(max_length=100,choices=CONTRACEPTIONS,default="rien",verbose_name="Contraception",null=True, blank=True)
+    patient_FCV_bool           = models.BooleanField(default=False, verbose_name="FCV", blank=True)
+    patient_FCV_com            = models.CharField(max_length=300, verbose_name="FCV informations", null=True, blank=True)
+    patient_addictions         = models.CharField(max_length=300, verbose_name="Tabac/addications", null=True, blank=True)
+    patient_allergies          = models.CharField(max_length=300, verbose_name="Allergie(s)", null=True, blank=True)
+
     slug                       = models.SlugField(null=True, blank=True)
-    patient_address            = models.CharField(max_length=300, verbose_name="Adresse")
-    patient_zip_code           = models.CharField(max_length=5, default="69960", verbose_name="Code postal")
-    patient_city               = models.CharField(max_length=64, default="Corbas", verbose_name="Ville")
-    patient_contry             = models.CharField(max_length=64, default="France", verbose_name="Pays")
     created_at                 = models.DateTimeField(auto_now_add=True)
     updated_at                 = models.DateTimeField(auto_now=True)
-    patient_note               = models.TextField(null=True, blank=True, default="Pas de remarques")
+
 
     class Meta:
         verbose_name = 'Patiente'
@@ -35,6 +77,9 @@ class Patient(models.Model):
 
     def get_adress(self):
         return self.patient_address + ", " + self.patient_zip_code + " " + self.patient_city + " - " + self.patient_contry
+
+    def get_imc(self):
+        return self.patient_poid / (self.patient_taille*self.patient_taille)
 
     def printable_number(self):
         number = self.patient_phone_number
